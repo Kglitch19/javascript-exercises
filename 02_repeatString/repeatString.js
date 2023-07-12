cc52a9824a6c7213a8e2dfc42bd645b203d99231
Khadeeja <khfarouk1593@outlook.com>
2023-07-12 17:54:32 +0300

    Write the function that passes the test,  to repeat a string thrice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function() {
-
+    string = "hey";
+    num = 3;
+    for (let i = 1; i < num; i++) {
+        string += "hey";
+    }
+    return string;
 };
 
 // Do not edit below this line
