c902eefbd96d6abc3805e1a75c13e7296cf3da72
Khadeeja <khfarouk1593@outlook.com>
2023-07-16 16:30:54 +0300

    Remove a single value from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+   for (let i = 0; i <= value.length-1 ; i++) {
+    array.splice(remove[i], 1);
+   }
+    return array;
 };
 
 // Do not edit below this line
