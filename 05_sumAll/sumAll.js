276419a0a54abab6c9cd0fc99315ce31a3df4950
Khadeeja <khfarouk1593@outlook.com>
2023-07-16 17:42:45 +0300

    Sum numbers within the range, even with larger numbers first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function(start, end) {
     let sum = 0;
+    let temp = 0;
+    if (start > end) {
+        // swapping values of variables
+        temp = start;
+        start = end;
+        end = temp;
+    }
+
+    // loops through the given parameter range
     for (let i = start; i <= end; i++) {
         sum += i;
     }
