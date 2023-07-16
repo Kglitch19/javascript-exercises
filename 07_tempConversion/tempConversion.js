b834537d411c06d4ca434d654cb62fb6985f1b69
Khadeeja <khfarouk1593@outlook.com>
2023-07-16 20:12:22 +0300

    Convert the temperature to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,7 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+
+  let cTemp = (fTemp - 32) * 5/9;
+  return cTemp;
 };
 
 const convertToFahrenheit = function() {
