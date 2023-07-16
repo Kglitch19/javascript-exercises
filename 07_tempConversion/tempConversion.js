fbd76e9c147d89f5531f008bb9f87b0ac4a59d10
Khadeeja <khfarouk1593@outlook.com>
2023-07-16 20:20:58 +0300

    Convert the temperature to fahrenheit and round it to 1 decimal, works with negative temperatures too

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -3,7 +3,8 @@ const convertToCelsius = function(fTemp) {
   return Math.round((fTemp - 32) * 5/9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  return Math.round(((cTemp * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
