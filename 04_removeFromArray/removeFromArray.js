8a67ea10530b82d963a568b869c1e2a9223a5579
Khadeeja <khfarouk1593@outlook.com>
2023-07-16 17:14:28 +0300

    Remove multiple values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,11 @@
-const removeFromArray = function(array, value) {
-   for (let i = 0; i <= value.length-1 ; i++) {
-    array.splice(remove[i], 1);
-   }
-    return array;
+const removeFromArray = function(array, ...values) {
+   const newArray = [];
+   array.forEach((item) => {
+    if (!values.includes(item)) {
+        newArray.push(item);
+    }
+   })
+    return newArray;
 };
 
 // Do not edit below this line
